c191f76a2568d363b6ac896a97af70f6c30e80dc
ortiz013 <122915945+ortiz013@users.noreply.github.com>
2026-06-16 19:54:10 -0700

    Delete pingpongscript.js

--- pingpongscript.js
+++
@@ -1,7 +0,0 @@
-// simple console log for now (expand later)
-console.log("UG Ping Pong Tournament loaded");
-
-// future ideas:
-// - highlight today's matches
-// - auto-refresh bracket
-// - countdown timer
